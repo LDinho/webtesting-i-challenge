c0bd73a6950eed25036020b7c553299b336d7d9a
LD <LDinho@users.noreply.github.com>
2019-06-25 20:38:09 -0400

    add mock item objects

---
+++ items/items.js
@@ -0,0 +1,24 @@
+
+const sword = {
+  name: "silver sword",
+  durability: 10,
+  enhancement: 5
+};
+
+const rock = {
+  name: "rocky",
+  durability: 10,
+  enhancement: 5
+};
+
+const shoe = {
+  name: "shoeless",
+  durability: 15,
+  enhancement: 2
+};
+
+module.exports = {
+  sword,
+  rock,
+  shoe,
+}
