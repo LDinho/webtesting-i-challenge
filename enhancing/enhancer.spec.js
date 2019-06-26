2eaef81c9cac5454f30207caa86db75cbb105a73
LD <LDinho@users.noreply.github.com>
2019-06-25 21:00:37 -0400

    add initial test suite set up & add repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,35 @@
 const enhancer = require('./enhancer.js');
-// test away!
+const items = require('../items/items.js');
+
+const {
+  sword,
+  rock,
+  shoe,
+} = items;
+
+const {
+  succeed,
+  fail,
+  repair,
+} = enhancer;
+
+describe('the enhancer', () => {
+
+  describe('the repair function', () => {
+
+    it('should restore durability to 100', () => {
+
+      // arrange - example data
+      const newSword = {...sword};
+
+      // act
+      const result = repair(newSword);
+
+      //assert
+      expect(result.durability).toBe(100);
+
+    });
+
+  });
+
+});
